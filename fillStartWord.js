06595a9f9cc4a1e907d70a709a8c0b9e2f449933
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-02 15:34:10 +0700

    output5v4

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,7 +1,7 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  if (startWord && word !== undefined & null) {
+  if (startWord && word !== (undefined | null)) {
     if (startWord === word.substring(0,startWord.length))
     return word
   else return startWord + word
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 function fillStartWord(startWord, word) {
-  if (startWord && word !== undefined & null) {
+  if (startWord && word !== (undefined | null)) {
     if (startWord === word.substring(0,startWord.length))
     return word
   else return startWord + word
   }
   return undefined
 }
-console.log(fillStartWord('fist',null))
+console.log(fillStartWord('fist','dsds'))
