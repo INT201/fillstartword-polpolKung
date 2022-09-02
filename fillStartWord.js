d2e8b5e7d3a7bd700eb75fd45780f2207ddb91fe
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-02 16:01:45 +0700

    output5v5

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,7 +1,8 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  if (startWord && word !== (undefined | null)) {
+  if ((startWord !== undefined && startWord !== null) && 
+    (word !== undefined && word !== null)) {
     if (startWord === word.substring(0,startWord.length))
     return word
   else return startWord + word
--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 function fillStartWord(startWord, word) {
-  if (startWord && word !== (undefined | null)) {
+  if ((startWord !== undefined && startWord !== null) && 
+    (word !== undefined && word !== null)) {
     if (startWord === word.substring(0,startWord.length))
     return word
   else return startWord + word
   }
   return undefined
 }
-console.log(fillStartWord('fist','dsds'))
+console.log(fillStartWord('fist', null ))
+console.log(fillStartWord('fist', undefined ))
+console.log(fillStartWord(undefined, 'null' ))
+console.log(fillStartWord('fist', 'null' ))
