b87a741903e0d264994fce7a9039528f9e8c59c3
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-02 15:16:15 +0700

    output4v3

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 function fillStartWord(startWord, word) {
-  if (startWord || word === undefined || null)
+  if (startWord | word === undefined || null)
     return undefined
   else if (startWord === word.substring(0,startWord.length))
     return word
   else return startWord + word
 }
-console.log(fillStartWord('fist',undefined))
+console.log(fillStartWord('fist','undefined'))
