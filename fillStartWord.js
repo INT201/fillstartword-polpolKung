c5e6bff105868131ed73008ac46cb238878f9475
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-02 14:45:35 +0700

    delete console.log

--- fillStartWord.js
+++ fillStartWord.js
@@ -3,5 +3,4 @@ const { template } = require('@babel/core')
 function fillStartWord(startWord, word) {
   return startWord + word
 }
-console.log(fillStartWord('123','dsd'))
 module.exports = fillStartWord
