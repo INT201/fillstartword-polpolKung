f6451c62110461323c0164765314c2d1d304fb12
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-02 15:17:28 +0700

    output5

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,7 +1,7 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  if (startWord | word === undefined || null)
+  if (startWord | word === undefined | null)
     return undefined
   else if (startWord === word.substring(0,startWord.length))
     return word
