86b5082692919cea904a6a671b4bd490f1fe0550
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-02 15:21:58 +0700

    output5v2

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,10 +1,11 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  if (startWord | word === undefined | null)
-    return undefined
-  else if (startWord === word.substring(0,startWord.length))
+  if (startWord && word !== undefined | null) {
+    if (startWord === word.substring(0,startWord.length))
     return word
   else return startWord + word
+  }
+  return undefined
 }
 module.exports = fillStartWord
